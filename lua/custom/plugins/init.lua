0c4bf5098c9e88949d596a53c01930ede6dd23f4
De-Rosa <lucadr222@gmail.com>
2025-02-28 23:52:44 +0000

    Initial changes

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,12 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'lervag/vimtex',
+    lazy = false, -- we don't want to lazy load VimTeX
+    init = function()
+      vim.g.vimtex_view_method = 'skim'
+    end,
+  },
+}
