74b5c2a6362557c006955c7e557a22fe8cc605b7
De-Rosa <lucadr222@gmail.com>
2025-03-01 15:34:52 +0000

    Changes to theme

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -21,7 +21,7 @@ require('lazy').setup({
 
   require 'kickstart/plugins/cmp',
 
-  require 'kickstart/plugins/tokyonight',
+  require 'kickstart/plugins/catppuccin',
 
   require 'kickstart/plugins/todo-comments',
 
